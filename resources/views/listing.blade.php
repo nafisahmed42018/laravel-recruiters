0e5215294b2ab8f8df81aab4435a967e6435cea7
nafisahmed42018 <nafisahmed@iut-dhaka.edu>
2022-12-15 23:55:04 +0600

    blade template without component

--- resources/views/listing.blade.php
+++ resources/views/listing.blade.php
@@ -1,6 +1,12 @@
+@extends('layout')
+
+@section('content')
+@include('partials._search')
 <h2>
     {{$job_listing['title']}}
 </h2>
 <p>
     {{$job_listing['description']}}
 </p>
+@endsection
+
--- resources/views/listings.blade.php
+++ resources/views/listings.blade.php
@@ -1,5 +1,7 @@
 @extends('layout')
 @section('content')
+@include('partials._hero')
+@include('partials._search')
     
 {{$heading}}</h1>
 <div class="lg:grid lg:grid-cols-2 gap-4 space-y-4 md:space-y-0 mx-4">
@@ -12,14 +14,14 @@
     <div class="flex">
         <img
             class="hidden w-48 mr-6 md:block"
-            {{-- src="images/acme.png" --}}
+            src="images/no-image.png"
             alt=""
         />
         <div>
             <h3 class="text-2xl">
-                <a href="show.html">Senior Laravel Developer</a>
+                <a href="/job-listings/{{$job_listing->id}}">{{$job_listing->title}}</a>
             </h3>
-            <div class="text-xl font-bold mb-4">Acme Corp</div>
+            <div class="text-xl font-bold mb-4">{{$job_listing->company}}</div>
             <ul class="flex">
                 <li
                     class="flex items-center justify-center bg-black text-white rounded-xl py-1 px-3 mr-2 text-xs"
@@ -43,8 +45,7 @@
                 </li>
             </ul>
             <div class="text-lg mt-4">
-                <i class="fa-solid fa-location-dot"></i> Boston,
-                MA
+                <i class="fa-solid fa-location-dot"></i> {{$job_listing->location}}
             </div>
         </div>
     </div>
